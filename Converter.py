ae602782ee85df85c1921e0f85c654f2621fff55
Valery Chumakou <vchumakou@gmail.com>
2024-02-06 09:45:06 -0500

    Alignment fixed

--- Converter.py
+++ Converter.py
@@ -7,14 +7,14 @@ c = Canvas(frame, bg="pink", height="400", width="500")
 frame.geometry("400x250")
 
 frame.resizable(0,0)
-cels = Label(frame, text = "C").place(x = 30, y = 50)
+cels = Label(frame, text = "Celsius", bg="pink").place(x = 90, y = 50)
 box1 = Entry(frame)
-box1.place(x = 80, y = 50)
+box1.place(x = 170, y = 50)
  
 
-far = Label(frame, text = "F").place(x = 30, y = 90)
+far = Label(frame, text = "Fahrenheit", bg="pink").place(x = 90, y = 90)
 box2 = Entry(frame)
-box2.place(x = 80, y = 90)
+box2.place(x = 170, y = 90)
 
 
 
@@ -38,10 +38,10 @@ def convertButton2():
       box2.delete(0,END)
       box2.insert(0,str(round(f,2)))
 
-convert = Button(frame, text = "Convert", command=convertButton)
-convert.place(x = 120, y = 150)
+convert = Button(frame, text = "Convert F to C", command=convertButton)
+convert.place(x = 90, y = 130)
 convert2 = Button(frame, text="Convert C to F", command=convertButton2)
-convert2.place(x=200, y = 140)
+convert2.place(x=200, y = 130)
  
 
 c.pack()
